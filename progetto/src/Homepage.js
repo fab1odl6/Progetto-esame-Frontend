3f6dc5d8e16a28bc2cf53942c29db5d545be82a2
fab10dl6 <fabiodilazzaro.fdl@gmail.com>
2023-12-28 15:09:47 +0100

    header condiviso aggiornato

---
+++ progetto/src/Homepage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function HomePage() {
+
+
+
+    return (
+        <div>
+            <div className="sectionHeader">
+                <div className="sectionElement">Homepage</div>
+                <div className="sectionElement">Every Artwork</div>
+                <div className="sectionElement">Museums</div>
+                <div className="sectionElement">Personal Gallery</div>
+                <div className="sectionElement">My Events</div>
+            </div>
+            <div className="searchBarHeader">
+                <div className="searchBar"></div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
