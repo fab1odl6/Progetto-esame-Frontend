732f68ff7ef9d4d496bcdff6fb4ab46644985b54
Giul98 <giulietta98.gr@gmail.com>
2023-12-29 16:06:50 +0100

    Base pagina Museums

--- progetto/src/App.js
+++ progetto/src/App.js
@@ -1,10 +1,12 @@
 import HomePage from "./HomePage/Homepage";
+import Museums from "./Museums/Museums";
+import 'bootstrap/dist/css/bootstrap.min.css';
 // Cambiare il return per testare
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Museums />
     </div>
   );
 }
