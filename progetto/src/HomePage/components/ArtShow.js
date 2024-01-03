ab1adec8c5547ad9928a6fb51d13a4da3a618b4c
fab10dl6 <fabiodilazzaro.fdl@gmail.com>
2024-01-03 12:38:32 +0100

    prova (fallita) di modale

--- progetto/src/HomePage/components/ArtShow.js
+++ progetto/src/HomePage/components/ArtShow.js
@@ -1,24 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
-import { switchFullArt } from "../store";
+import { switchFullArt, switchFavoriteArt } from "../store";
 import { IoIosClose } from "react-icons/io";
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 
 function ArtShow() {
 
-    const { array, index } = useSelector((state) => {
+    const { array, index, full } = useSelector((state) => {
         return state.artworks;
     })
 
     const dispatch = useDispatch();
 
-    const handleClick = function () {
+    const handleClickClose = function () {
         dispatch(switchFullArt());
     }
 
+    const handleClickHeart = function () {
+        dispatch(switchFavoriteArt());
+    }
+
     return (
-        <div className="fullShow">
-            <IoIosClose className="closeButton" onClick={handleClick} />
-            <div>{array[index].name}</div>
+        <div className="open">
+            <IoIosClose className="closeButton" onClick={handleClickClose} />
+            <div className="firstRow">
+                <div>{array[index].name}</div>
+                {array[index].favorite ? (
+                    <FaHeart className="favoriteShow" onClick={handleClickHeart} />
+                ) : (
+                    <FaRegHeart className="favoriteShow" onClick={handleClickHeart} />
+                )}
+            </div>
             <div>bbb</div>
         </div>
     )
