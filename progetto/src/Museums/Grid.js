a6a4def93e6e7a5f796a6fdb3d62c67aed0fa0f6
Giul98 <giulietta98.gr@gmail.com>
2024-01-10 15:26:10 +0100

    Connessione Db con i dipartimenti

--- progetto/src/Museums/Grid.js
+++ progetto/src/Museums/Grid.js
@@ -1,23 +1,3 @@
-/* Grid.js
-import React from 'react';
-
-function Grid({ museums, openModal }) {
-  return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {museums.map((museum, index) => (
-        <div key={museum.id} style={{ flex: '0 0 50%', boxSizing: 'border-box', padding: '5px' }} onClick={() => openModal(museum)}>
-          <img src={museum.image} alt={museum.displayName} style={{ width: '100%', borderRadius: '8px' }} />
-          <h3>{museum.displayName}</h3>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default Grid;
-
-*/
-
 // Grid.js
 import React from 'react';
 import ImageBox from './ImageBox';
