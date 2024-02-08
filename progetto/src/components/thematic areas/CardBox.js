17838dfdfe9f943d7a114975b66eaa9ef8f6bda0
Davide <sciarradavide@gmail.com>
2024-02-08 17:27:11 +0100

    css

--- progetto/src/components/thematic areas/CardBox.js
+++ progetto/src/components/thematic areas/CardBox.js
@@ -6,8 +6,8 @@ function CardBox({ imageUrl, name, onClick }) {
     width: "100%",
     height: "110%",
   };
-  const textStyle = "w-full p-4 font-bold text-2xl text-#444455 text-shadow-lg relative z-10";
-  const transparentAreaStyle ="absolute bottom-0 left-0 w-full h-16 bg-opacity-80 bg-gray-300 z-0 pb-4 pointer-events-none"; // Applica "pointer-events: none;" qui
+  const textStyle = "w-full p-4 font-bold text-2xl text-#444455 text-shadow-lg relative z-10 -mb-2";
+  const transparentAreaStyle ="absolute bottom-0 left-0 w-full h-16 bg-opacity-80 bg-gray-300 z-0 pb-4 pointer-events-none "; // Applica "pointer-events: none;" qui
   const backgroundcontainer = "relative overflow-hidden aspect-video bg-red-400 cursor-pointer rounded-xl group";
   const textClass = "absolute inset-0 flex items-end";
   const imageboxClass = "object-cover w-full h-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out";
