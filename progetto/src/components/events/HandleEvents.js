373fbc183b5b700c3f104ae2229fba64482ad34f
fab10dl6 <fabiodilazzaro.fdl@gmail.com>
2024-02-09 15:27:41 +0100

    aaa

--- progetto/src/components/events/HandleEvents.js
+++ progetto/src/components/events/HandleEvents.js
@@ -6,11 +6,13 @@ import { useSelector } from "react-redux";
 import { useEffect, useState, useRef } from "react";
 
 function HandleEvents() {
-  const containerClass ="max-w-screen-md min-w-screen-md h-full p-4";
-  const titleClass ="text-2xl font-bold mb-4 mt-2 mx-auto text-center text-[#444455]";
+  const containerClass = "max-w-screen-md min-w-screen-md h-full p-4";
+  const titleClass =
+    "text-2xl font-bold mb-4 mt-2 mx-auto text-center text-[#444455]";
   const emptyContainerClass = "custom-message-container max-w-md min-w-md";
   const textClass = "custom-message-text text-[#444455]";
-  const eventContainer ="max-h-100 overflow-y-auto h-full p-4 mt-4 shadow rounded border-2 border-[#77aaff]"
+  const eventContainer =
+    "max-h-100 overflow-y-auto h-full p-4 mt-4 shadow rounded border-2 border-[#77aaff]";
 
   const app = initializeApp(firebaseConfig);
   const db = getDatabase();
@@ -52,12 +54,12 @@ function HandleEvents() {
     <div className={containerClass}>
       <div className={titleClass}>CUSTOM EVENTS</div>
       <div
-        className={eventContainer} 
-        style={{ maxHeight: '600px' }}
+        className={eventContainer}
+        style={{ maxHeight: "600px" }}
         ref={eventsContainerRef}
       >
         {localEvents.length > 0 ? (
-          localEvents.map((event, index) => (
+          localEvents.map((event) => (
             <div className="mt-4" key={event.name}>
               <HandleEventCard key={event.name} event={event} />
             </div>
